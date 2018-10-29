39ce115b75f576665e879ed74d85c4bc8c0993ac
Ben Kelly <ben.kelly@gmail.com>
2018-10-29 07:04:06 +0000

    routes AFTER config, otherwise you get nasty shit like req.body == empty

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 
 require('./startup/logging')(app);
-require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/frontend')(app);
 require('./startup/config')();
 require('./startup/validation');
+require('./startup/routes')(app);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`listening on port ${port}`));
 
 module.exports = server;
