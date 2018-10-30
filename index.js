3ece41207722d3b1228e458614cf2e52961d9fb5
Ben Kelly <ben.kelly@gmail.com>
2018-10-30 06:49:00 +0000

    prep for production

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./startup/frontend')(app);
 require('./startup/config')();
 require('./startup/validation');
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`listening on port ${port}`));
